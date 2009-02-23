1bfadf1f08fa0818de1ecdceafb024e3c8820556
Glen Robson <glen.robson@unknown.com>
2009-02-23 21:30:24 +0000

    Moved code away from httpunit to apache httpclient as I am only testing http not html so httpunit a bit of an over kill.

--- src/test/TestServiceDocument.java
+++ src/test/TestServiceDocument.java
@@ -1,12 +1,14 @@
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebRequest;
-import com.meterware.httpunit.GetMethodWebRequest;
-import com.meterware.httpunit.WebResponse;
-
 import java.io.IOException;
-import org.xml.sax.SAXException;
 
-import org.jdom.input.DOMBuilder;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+
+import org.jdom.input.SAXBuilder;
 import org.jdom.Document;
 import org.jdom.output.XMLOutputter;
 import org.jdom.output.Format;
@@ -22,19 +24,37 @@ public class TestServiceDocument {
 	private Namespace SWORD = Namespace.getNamespace("sword","http://purl.org/net/sword/");
 	private Namespace ATOM = Namespace.getNamespace("atom","http://www.w3.org/2005/Atom");
 	private Namespace APP = Namespace.getNamespace("app","http://www.w3.org/2007/app");
-	//private Namespace APP = Namespace.getNamespace("app","http://purl.org/atom/app#");
+
+	protected String _serviceDocURL = "";
 
 	public TestServiceDocument() {
+			_serviceDocURL = System.getProperty("service_doc");
+	}
+	
+	protected HttpClient getClient() {
+		HttpClient tClient = new HttpClient();
+		tClient.getParams().setAuthenticationPreemptive(true);
+
+		Credentials tUserPass = new UsernamePasswordCredentials("sword", "sword");
+		tClient.getState().setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM), tUserPass);
+	
+		return tClient;
+	}
+
+	protected Document getServiceDoc() throws IOException, JDOMException {
+		GetMethod tMethod = new GetMethod(_serviceDocURL);
+
+		int tStatus = this.getClient().executeMethod(tMethod);
+
+		assertEquals("Status non 200", 200, tStatus);
+
+		SAXBuilder tBuilder = new SAXBuilder();
+		return tBuilder.build(tMethod.getResponseBodyAsStream());
 	}
 
 	@Test
-	public void testMediatedDeposit() throws IOException, JDOMException, SAXException {
-		WebConversation tWC = new WebConversation();
-		tWC.setAuthentication("SWORD", "sword", "sword");
-		WebRequest tReq = new GetMethodWebRequest( "http://localhost/sword/servicedocument" );
-		WebResponse tResp = tWC.getResponse(tReq);
-		DOMBuilder tBuilder = new DOMBuilder();
-		Document tServiceDoc = tBuilder.build(tResp.getDOM());
+	public void testMediatedDeposit() throws IOException, JDOMException {
+		Document tServiceDoc = this.getServiceDoc();
 
 		XPath tPath = XPath.newInstance("/app:service/app:workspace/app:collection/sword:mediation");
 		tPath.addNamespace(APP);
@@ -44,7 +64,7 @@ public class TestServiceDocument {
 		assertTrue("sword:mediation must contain either true or false", tMediation.getText().equals("true") || tMediation.getText().equals("false"));
 	}
 
-	public static void main(final String pArgs[]) throws IOException, SAXException {
+	public static void main(final String pArgs[]) throws IOException {
 		org.junit.runner.JUnitCore.main(TestServiceDocument.class.getName());
 	}
 }
