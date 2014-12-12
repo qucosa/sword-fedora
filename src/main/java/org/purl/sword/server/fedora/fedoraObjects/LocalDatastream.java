f8811c37be3707e0fc946818b5f9dee7a97d4418
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2014-12-12 11:15:07 +0100

    A bit of code cleanup

--- src/main/java/org/purl/sword/server/fedora/fedoraObjects/LocalDatastream.java
+++ src/main/java/org/purl/sword/server/fedora/fedoraObjects/LocalDatastream.java
@@ -1,48 +1,41 @@
-package org.purl.sword.server.fedora.fedoraObjects;
-
 /**
-  * Copyright (c) 2007, Aberystwyth University
-  *
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions
-  * are met:
-  *
-  *  - Redistributions of source code must retain the above
-  *    copyright notice, this list of conditions and the
-  *    following disclaimer.
-  *
-  *  - Redistributions in binary form must reproduce the above copyright
-  *    notice, this list of conditions and the following disclaimer in
-  *    the documentation and/or other materials provided with the
-  *    distribution.
-  *
-  *  - Neither the name of the Centre for Advanced Software and
-  *    Intelligent Systems (CASIS) nor the names of its
-  *    contributors may be used to endorse or promote products derived
-  *    from this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
-  * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-  * SUCH DAMAGE.
-  *
-  * @author Glen Robson
-  * @version 1.0
-  * Date: 18 October 2007 
-  *
-  * This encapsualtes datastreams which are on the local file system
-  * and need to be uploaded to fedora. These files are set to a Managed state
-  */
+ * Copyright (c) 2007, Aberystwyth University
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the
+ *    following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *  - Neither the name of the Centre for Advanced Software and
+ *    Intelligent Systems (CASIS) nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+package org.purl.sword.server.fedora.fedoraObjects;
 
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.Header;
@@ -64,20 +57,27 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+/**
+ * This encapsulates datastreams which are on the local file system
+ * and need to be uploaded to fedora. These files are set to a Managed state
+ *
+ * @author Glen Robson
+ * @version 1.0
+ * @since 18 October 2007
+ */
 public class LocalDatastream extends Datastream {
 	private static final Logger LOG = Logger.getLogger(LocalDatastream.class);
-	protected String _path = "";
-	protected String _uploadedURL = null;
+	private String _path = "";
+	private String _uploadedURL = null;
 
 	/**
-	 * @param String datastream name 
-	 * @param String Mime type
-	 * @param String the absolute path for the datastream
+	 * @param pID       Datastream ID
+	 * @param pMimeType Mime type
+	 * @param pPath     The absolute path for the datastream
 	 */
 	public LocalDatastream(final String pID, final String pMimeType, final String pPath) {
 		super(pID, State.ACTIVE, ControlGroup.MANAGED, pMimeType);
 		super.setLabel("SWORD Generic File Upload");
-		
 		this.setPath(pPath);
 	}
 
@@ -159,19 +159,18 @@ public class LocalDatastream extends Datastream {
 	}
 
 	/**
-	 * Converts this datastream into FOXML so it can be ingested. 
-	 * @param Namespace the FOXML namespace
+	 * Converts this datastream into FOXML so it can be ingested.
+	 *
+	 * @param namespace The FOXML namespace
 	 * @return Element the FOXML datastream node
 	 */
-	public Element dsToFOXML(final Namespace FOXML) {
+	public Element dsToFOXML(final Namespace namespace) {
 		if (_uploadedURL == null) {
 			throw new IllegalArgumentException("Please upload the datastream before ingesting");
 		}
-		
-		Element tContentLocation = new Element("contentLocation", FOXML);
+		Element tContentLocation = new Element("contentLocation", namespace);
 		tContentLocation.setAttribute("TYPE", "URL");
 		tContentLocation.setAttribute("REF", _uploadedURL);
-
 		return tContentLocation;
-	}	
+	}
 }
